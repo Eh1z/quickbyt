c09acb3a60bb3caabd339ce1c50ccb97a4ad1a8c
Eh1z <godsentehiz11@gmail.com>
2026-03-21 11:15:44 +0100

    feat: implement homepage layout with offers list and cart button component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,89 @@
-import { Text } from "react-native";
+import { offers } from "@/constants";
+import cn from "clsx";
+import React from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { CartButton } from "../components";
+import { images } from "../constants";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white p-4">
-      <Text className="text-7xl  text-primary font-quicksand-bold">
-        Welcome to QuickByt!
-      </Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        contentContainerClassName="pb-24 px-4"
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row",
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff2" }}
+              >
+                {({ pressed }) => (
+                  <React.Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        resizeMode={"contain"}
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10 " : "pl-10",
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="white"
+                      />
+                    </View>
+                  </React.Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-4 ">
+            <View className="flex-start">
+              <Text className="small-bold text-primary text-center mt-5 uppercase">
+                Deliver To
+              </Text>
+              <TouchableOpacity className="flex-row flex-center gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100 uppercase">
+                  Shinjuku
+                </Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 }
