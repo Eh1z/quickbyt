0b187ecd039dd60ab347926a822c89e1d71e8503
Eh1z <godsentehiz11@gmail.com>
2026-03-22 07:33:20 +0100

    feat: update tab layout to use NativeTabs and refactor components for consistency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import useAuthStore from "@/lib/auth.store";
-import { Redirect, Tabs } from "expo-router";
-// import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Redirect } from "expo-router";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
 
 export default function Tablayout() {
@@ -9,43 +9,25 @@ export default function Tablayout() {
   if (!isAuthenticated) {
     return <Redirect href={"/sign-in"} />;
   }
+
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-        }}
-      />
-      <Tabs.Screen
-        name="cart"
-        options={{
-          title: "Cart",
-        }}
-      />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="search">
+        <Icon sf="magnifyingglass" drawable="custom_search_drawable" />
+        <Label>Search</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="cart">
+        <Icon sf="cart" drawable="custom_cart_drawable" />
+        <Label>Cart</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <Icon sf="person" drawable="custom_profile_drawable" />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
-
-// <NativeTabs>
-//       <NativeTabs.Trigger name="index">
-//         <Label>Home</Label>
-//         <Icon sf="house.fill" drawable="custom_android_drawable" />
-//       </NativeTabs.Trigger>
-//       <NativeTabs.Trigger name="settings">
-//         <Icon sf="gear" drawable="custom_settings_drawable" />
-//         <Label>Settings</Label>
-//       </NativeTabs.Trigger>
-//     </NativeTabs>
