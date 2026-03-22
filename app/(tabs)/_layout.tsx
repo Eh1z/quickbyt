b51ac5dfec4972ef3847f558bf122a1980748b05
Eh1z <godsentehiz11@gmail.com>
2026-03-22 07:21:19 +0100

    feat: implement tab navigation in layout component and replace Slot with Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import useAuthStore from "@/lib/auth.store";
-import { Redirect, Slot } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
+// import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
 
 export default function Tablayout() {
@@ -8,5 +9,43 @@ export default function Tablayout() {
   if (!isAuthenticated) {
     return <Redirect href={"/sign-in"} />;
   }
-  return <Slot />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+        }}
+      />
+    </Tabs>
+  );
 }
+
+// <NativeTabs>
+//       <NativeTabs.Trigger name="index">
+//         <Label>Home</Label>
+//         <Icon sf="house.fill" drawable="custom_android_drawable" />
+//       </NativeTabs.Trigger>
+//       <NativeTabs.Trigger name="settings">
+//         <Icon sf="gear" drawable="custom_settings_drawable" />
+//         <Label>Settings</Label>
+//       </NativeTabs.Trigger>
+//     </NativeTabs>
