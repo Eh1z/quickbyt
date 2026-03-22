c816c0921b2a27caa06452b9826e3dddc6bee0a4
Eh1z <godsentehiz11@gmail.com>
2026-03-22 09:31:06 +0100

    feat: add seed functionality and dummy data for categories, customizations, and menu items

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,17 @@
+import seed from "@/lib/seed";
 import React from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 const Search = () => {
   return (
     <SafeAreaView>
       <Text>Search</Text>
+      <Button
+        title="Seed Data"
+        onPress={() =>
+          seed().catch((error) => console.error("Failed to seed data:", error))
+        }
+      />
     </SafeAreaView>
   );
 };
